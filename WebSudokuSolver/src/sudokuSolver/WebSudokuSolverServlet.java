d8c11a81daf3bc823636a4f702be7f19e344202e
mikaylathompson <mikayla.thompson@yale.edu>
2013-03-29 10:49:13 -0400

    Foundation Google App Defaults

---
+++ WebSudokuSolver/src/sudokuSolver/WebSudokuSolverServlet.java
@@ -0,0 +1,11 @@
+package sudokuSolver;
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class WebSudokuSolverServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
